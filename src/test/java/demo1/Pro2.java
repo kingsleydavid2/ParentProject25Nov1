3290660b9f4a7d033967593dc868abe9aa8ead8f
Mohan S <kingsleydavid.s@thestreet.com>
2017-11-29 09:55:00 +0530

    29Nov2017-New Update

---
+++ src/test/java/demo1/Pro2.java
@@ -0,0 +1,12 @@
+package demo1;
+
+public class Pro2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Kingsley, Welcome Back..ALL THE BEST");
+
+	}
+
+}
